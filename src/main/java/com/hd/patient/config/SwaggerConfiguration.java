e48a192aeb2b6ac0b95b4f184ae9566cafe3250f
RadSparkle <wwwartg1@naver.com>
2023-05-24 19:09:00 +0900

    Chore : Swagger Configuration 추가

---
+++ src/main/java/com/hd/patient/config/SwaggerConfiguration.java
@@ -0,0 +1,44 @@
+package com.hd.patient.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfiguration {
+
+    private static final String API_NAME = "Patient-management API";
+    private static final String API_VERSION = "0.0.1 Verstion";
+    private static final String API_DESCRIPTION = "환자 관리 API 명세서";
+
+    private static final String BASE_PACKAGE = "com.hd.patient.api";
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(mySwaggerInfo());
+    }
+    @Bean
+    public InternalResourceViewResolver defaultViewResolver() {
+        return new InternalResourceViewResolver();
+    }
+    private ApiInfo mySwaggerInfo() {
+        return new ApiInfoBuilder()
+                .title(API_NAME)
+                .description(API_DESCRIPTION)
+                .version(API_VERSION)
+                .build();
+    }
+}
