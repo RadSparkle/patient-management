bb1b45268a411eacd945203a5a6ba1d4b96ee038
RadSparkle <wwwartg1@naver.com>
2023-05-26 03:58:20 +0900

    Chore : 검색키워드와 검색조건 타입이 들어있는 vo 생

--- src/main/java/com/hd/patient/api/search/model/SearchVo.java
+++ src/main/java/com/hd/patient/api/search/model/SearchVo.java
@@ -1,14 +1,17 @@
 package com.hd.patient.api.search.model;
 
 import com.hd.patient.enums.SearchEnum;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.ToString;
 
 @Data
 @ToString
 public class SearchVo {
+    @ApiModelProperty(value = "검색 키워드", example = "홍길동")
     private String keyword;
 
+    @ApiModelProperty(value = "검색 조건 타입", example = "name")
     private SearchEnum type;
 
     public SearchVo(String keyword, SearchEnum type) {
