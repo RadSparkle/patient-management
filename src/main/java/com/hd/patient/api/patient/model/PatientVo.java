241b2d17836dd53ff3418aa1c970d1776de77e4c
RadSparkle <wwwartg1@naver.com>
2023-05-26 00:33:32 +0900

    Chore : 방문정보를 담을 속성 추가, 검색쿼리를 위한 QueryProjection 추가

--- src/main/java/com/hd/patient/api/patient/model/PatientVo.java
+++ src/main/java/com/hd/patient/api/patient/model/PatientVo.java
@@ -1,8 +1,10 @@
 package com.hd.patient.api.patient.model;
 
+import com.querydsl.core.annotations.QueryProjection;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.util.Objects;
 
 @Data
 @NoArgsConstructor
@@ -27,4 +29,30 @@ public class PatientVo {
 
     @ApiModelProperty(value = "환자 휴대폰 번호",example = "01099994444")
     private String phoneNum;
+
+    @ApiModelProperty(value = "방문 날짜", example = "20230511")
+    private String visitDate;
+
+    @ApiModelProperty(value = "환자방문 ID", example = "1")
+    private Long visitId;
+
+    @ApiModelProperty(value = "방문상태코드", example = "1")
+    private String visitState;
+
+    @QueryProjection
+    public PatientVo(Long patientId, String patientNm, Long hospitalId, String gender, String patientRegNum
+            ,String birth, String phoneNum, String visitDate, String visitState, Long visitId){
+        this.patientId = patientId;
+        this.patientNm = patientNm;
+        this.patientRegNum =  patientRegNum;
+        this.gender = gender;
+        this.birth = birth;
+        this.phoneNum = phoneNum;
+        this.hospitalId = hospitalId;
+        if (Objects.nonNull(visitId)) {
+            this.visitId = visitId;
+            this.visitDate = visitDate;
+            this.visitState = visitState;
+        }
+    }
 }
